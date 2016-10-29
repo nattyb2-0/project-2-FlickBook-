dc9ede7b7de93bb2476ed863e399a2ed2bd18c54
Natty Davis <natty.davis79@gmail.com>
2016-10-28 22:51:20 -0400

    set up basic login page

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,17 @@ const express = require('express');
 
 const homeRoute = express.Router();
 
-// This is the route that serves your '/' homepage
+// when the homepage is called, render the index page on screen
 homeRoute.get('/', (req, res) => {
   res.render('index');
 });
+// when the login clicked, render the login page to user
+homeRoute.get('/login', (req, res) => {
+  res.render('login');
+});
+
+
+
 
 
 module.exports = homeRoute;
